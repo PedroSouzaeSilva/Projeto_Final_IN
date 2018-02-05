aa480a3932651228e53e048087016b11942648f1
Mynssem <rafaelmynssem@id.uff.br>
2018-02-05 14:38:03 -0200

    icone do trello adicionado

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -71,6 +71,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :deadline, :price, :content)
+      params.require(:project).permit(:name, :deadline, :price, :content, :trello)
     end
 end
