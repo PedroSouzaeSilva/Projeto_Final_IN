5bca9e725f62f090963189827b2404e5f1572086
Mynssem <rafaelmynssem@id.uff.br>
2018-02-06 16:18:08 -0200

    não importa

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -19,4 +19,39 @@ class DirectionsController < ApplicationController
     #@task = Task.new
   end
 
+  def marketing
+    d = Direction.find_by(name: "Diretoria de Marketing")
+    @acessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
+    #Carrega todos os usuarios da dietoria
+    #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
+    #@tarefas = Task.where(directorship_id: 3).order(deadline: :desc).paginate(:page => params[:page], :per_page => 10)
+    #@task = Task.new
+  end
+
+  def financeiro
+    d = Direction.find_by(name: "Diretoria de Financeiro")
+    @acessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
+    #Carrega todos os usuarios da dietoria
+    #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
+    #@tarefas = Task.where(directorship_id: 3).order(deadline: :desc).paginate(:page => params[:page], :per_page => 10)
+    #@task = Task.new
+  end
+
+  def projeto
+    d = Direction.find_by(name: "Diretoria de Projetos")
+    @acessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
+    #Carrega todos os usuarios da dietoria
+    #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
+    #@tarefas = Task.where(directorship_id: 3).order(deadline: :desc).paginate(:page => params[:page], :per_page => 10)
+    #@task = Task.new
+  end
+
+  def presidencia
+    d = Direction.find_by(name: "Diretoria de Presidência")
+    @acessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
+    #Carrega todos os usuarios da dietoria
+    #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
+    #@tarefas = Task.where(directorship_id: 3).order(deadline: :desc).paginate(:page => params[:page], :per_page => 10)
+    #@task = Task.new
+  end
 end
