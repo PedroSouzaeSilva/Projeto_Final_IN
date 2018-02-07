789ca61cd0346df0e6aec5c3907acff26b504efa
Pedro Souza <pedrosouzaesilva@id.uff.br>
2018-02-07 01:51:34 +0000

    Fim do projeto

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -20,7 +20,7 @@ class DirectionsController < ApplicationController
   end
 
   def marketing
-    d = Direction.find_by(name: "Diretoria de Marketing")
+    d = Direction.find_by(name: "Diretoria de Gerência de Marketing")
     @assessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
     #Carrega todos os usuarios da dietoria
     #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
@@ -29,7 +29,7 @@ class DirectionsController < ApplicationController
   end
 
   def financeiro
-    d = Direction.find_by(name: "Diretoria de Financeiro")
+    d = Direction.find_by(name: "Diretoria de Gerência de Financeiro")
     @assessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
     #Carrega todos os usuarios da dietoria
     #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
@@ -38,7 +38,7 @@ class DirectionsController < ApplicationController
   end
 
   def projetos
-    d = Direction.find_by(name: "Diretoria de Projetos")
+    d = Direction.find_by(name: "Diretoria de Gerência de Projetos")
     @assessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
     #Carrega todos os usuarios da dietoria
     #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
@@ -47,7 +47,7 @@ class DirectionsController < ApplicationController
   end
 
   def presidencia
-    d = Direction.find_by(name: "Diretoria de Presidência")
+    d = Direction.find_by(name: "Diretoria de Gerência de Presidência")
     @assessores=User.find_by_sql("select users.* from users where users.id in (select user_directions.user_id from user_directions where user_directions.direction_id = #{d.id})")
     #Carrega todos os usuarios da dietoria
     #@diretor = @acessores.where(manager: true)[0] #carrega aquele que e da diretoria e tem o acessor como treu
